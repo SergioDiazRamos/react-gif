68253822486dc9aa05989bdc4292d54be8d2ed08
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-19 11:51:19 +0100

    78. Comunicación entre componentes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,8 +5,8 @@ import { AddCategory } from './components';
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  const onAddCategory = () => {
-    setCategories(['Valorant', ...categories]);
+  const onAddCategory = (category) => {
+    setCategories([category, ...categories]);
   };
 
   return (
@@ -15,10 +15,10 @@ export const GifExpertApp = () => {
       <h1>GifExpertApp</h1>
 
       {/* Input */}
-      <AddCategory />
+      <AddCategory onAddCategory={onAddCategory} />
 
       {/* Listado de Gif */}
-      <button onClick={onAddCategory}>Agregar</button>
+      {/* <button onClick={onAddCategory}>Agregar</button> */}
       <ol>
         {categories.map((category, idx) => (
           <li key={idx}>{category}</li>
