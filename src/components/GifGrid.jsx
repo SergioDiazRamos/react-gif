bbdc416b1022892af5b91589c71fcc1193491af4
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-20 10:25:03 +0100

    83. useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
+
 import { getGifs } from '../helpers';
 
 export const GifGrid = ({ category }) => {
-  getGifs('valorant');
+  const [counter, setCounter] = useState(10);
+  useEffect(() => {
+    getGifs('valorant');
+  }, []);
 
   return (
     <>
       <h3>{category}</h3>
+
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
   );
 };
