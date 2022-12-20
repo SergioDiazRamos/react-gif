43a4f87bc1d559bb232f4cec9e793e10f41fe9ee
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-20 10:58:44 +0100

    85. Mostrar los títulos de las imágenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getGifs } from '../helpers';
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
   useEffect(() => {
-    getGifs('valorant');
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 const apiKey = 'IV9QAnFRAiAJMVyGrZ4f08PWtWPjpYaP';
 
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
   const gifs = data.map(({ id, title, images }) => ({
