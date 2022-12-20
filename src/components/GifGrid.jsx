d0ada2d1b2909993b5f92b4c838bf0e080ffcdbe
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-20 16:47:34 +0100

    88. Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,6 +4,8 @@ import { useFetchGifs } from '../hooks';
 export const GifGrid = ({ category }) => {
   const { images, isLoading } = useFetchGifs(category);
 
+  if (isLoading) return <h3>Cargando...</h3>;
+
   return (
     <>
       <h3>{category}</h3>
