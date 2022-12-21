154ea4be625a54c1222bfa78176f6a8e202294ee
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-21 19:27:59 +0100

    107. Jest Functions

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -13,14 +13,29 @@ describe('Pruebas en <AddCategory/>', () => {
   });
 
   test('Debe de llanar onNewCategory si el input tiene un valor', () => {
-    // TODO:
-    render(<AddCategory onNewCategory={() => {}} />);
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form');
 
     fireEvent.input(input, { target: { value: inputText } });
     fireEvent.submit(form);
     expect(input.value).toBe('');
-    // screen.debug();
+
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputText);
+  });
+
+  test('No debe de llamar el onNewCategory si el input está vacio', () => {
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+    const form = screen.getByRole('form');
+
+    fireEvent.submit(form);
+    // expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalled();
   });
 });
