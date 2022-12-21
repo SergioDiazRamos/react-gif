f7901fe28526d74d8fa4f0e2c96c18422365059d
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-21 18:47:47 +0100

    105. Pruebas del componente - AddCategory

---
+++ tests/components/AddCategory.test.jsx
@@ -0,0 +1,14 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { AddCategory } from '../../src/components/AddCategory';
+
+const inputText = 'Saitama';
+
+describe('Pruebas en <AddCategory/>', () => {
+  test('Debe de cambiar el valor de la caja de texto', () => {
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole('textbox');
+    fireEvent.input(input, { target: { value: inputText } });
+    expect(input.value).toBe(inputText);
+  });
+});
