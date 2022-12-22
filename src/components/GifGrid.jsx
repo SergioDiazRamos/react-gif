ade660c185ea66d92abc635264a87b2d5a905f23
Sergio Díaz <sergiodiazramos@gmail.com>
2022-12-22 18:51:14 +0100

    111. Pruebas de tarea

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -6,11 +6,11 @@ import { useFetchGifs } from '../hooks';
 export const GifGrid = ({ category }) => {
   const { images, isLoading } = useFetchGifs(category);
 
-  if (isLoading) return <h3>Cargando...</h3>;
-
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,11 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { GifExpertApp } from '../src/GifExpertApp';
 
 describe('Pruebas en <GifExpertApp/>', () => {
-  // TODO: Hacer más adelante 111. Pruebas de tarea, descripción de la tarea
-  test('description', () => {
+  test('Hace match con el Snapshot.', () => {
+    const { container } = render(<GifExpertApp />);
+    // screen.debug();
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Debe agregar una categoría si no existe', () => {
     render(<GifExpertApp />);
-    screen.debug();
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('textbox');
+
+    fireEvent.input(input, { target: { value: 'Saitama' } });
+    fireEvent.submit(form);
+
+    expect(screen.getByText('Saitama')).toBeTruthy();
+  });
+
+  test('No debe agregar una categoría si existe', () => {
+    render(<GifExpertApp />);
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('textbox');
+
+    fireEvent.input(input, { target: { value: 'One Punch' } });
+    fireEvent.submit(form);
+
+    expect(screen.getAllByRole('heading', { level: 3 }).length).toBe(1);
   });
 });
